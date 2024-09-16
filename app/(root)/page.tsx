837cfb273fce83a2acaed0b6834eb90915b42519
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-09-16 23:26:09 +0530

    homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import { podcastData } from '@/constants';
+import PodcastCard from '@/components/PodcastCard';
 import React from 'react';
 
 const Home = () => {
@@ -5,6 +7,9 @@ const Home = () => {
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        {podcastData.map((podcast) => (
+          <PodcastCard />
+        ))}
       </section>
     </div>
   );
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PodcastCard = () => {
+  return <div>PodcastCard</div>;
+};
+
+export default PodcastCard;
