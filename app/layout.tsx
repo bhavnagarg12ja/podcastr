a75c2187b4858e4548635f97520f33114b662aa1
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-09-23 22:51:41 +0530

    clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 import './globals.css';
-import { ConvexClientProvider } from './providers/ConvexClientProvider';
+import ConvexClerkProvider from './providers/ConvexClerkProvider';
 
 const manrope = Manrope({ subsets: ['latin'] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.className}`}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClerkProvider>{children}</ConvexClerkProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://precious-falcon-47.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+export default authConfig;
